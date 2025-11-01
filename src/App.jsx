8303f749b5ab4db1207bf3be0a6fec55663a5397
Samer411 <samer.nassim.behery@gmail.com>
2025-11-01 23:05:52 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,54 @@
+import { useState } from 'react';
 import Die from './components/Die';
+import { nanoid } from 'nanoid';
 
 export default function App() {
+  function generateAllNewDice() {
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return arr;
+  }
+  function selectDie(id) {
+    setDieValue((prevDieValue) =>
+      prevDieValue.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+  const [dieValue, setDieValue] = useState(generateAllNewDice());
+
   return (
     <body className="flex justify-center bg-[#0B2434]">
-      <main className="align-items m-10 flex h-150 w-150 flex-col items-center justify-center rounded-md bg-[#F5F5F5] p-10">
+      <main className="m-10 flex h-150 w-150 flex-col items-center space-y-3 rounded-md bg-[#F5F5F5] p-10">
         <h1 className="text-5xl">Tenzies</h1>
         <p className="text-2xl text-[#4A4E74]">
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </p>
         <div className="grid grid-cols-5">
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={4} />
-          <Die value={5} />
-          <Die value={6} />
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={4} />
+          {dieValue.map((ob) => (
+            <Die
+              key={ob.id}
+              value={ob.value}
+              isHeld={ob.isHeld}
+              onClick={() => {
+                selectDie(ob.id);
+              }}
+            />
+          ))}
         </div>
+        <button
+          className="rounded-md bg-[#5035FF] px-12 py-3 text-2xl font-bold text-white hover:cursor-pointer"
+          onClick={() => setDieValue(generateAllNewDice())}
+        >
+          Roll
+        </button>
       </main>
     </body>
   );
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,17 +1,9 @@
-import { use, useState } from 'react';
-
 export default function Die(props) {
-  function generateAllNewDice() {
-    let arr = [];
-    for (let i = 0; i < 10; i++) {
-      arr.push(Math.ceil(Math.random() * 6));
-    }
-    return arr;
-  }
-  const [dieValue, setDieValue] = useState(generateAllNewDice());
   return (
-    <button className="m-5 flex items-center justify-center rounded-md bg-white px-10 py-8 text-3xl font-bold hover:cursor-pointer hover:bg-[#59E391]">
-      {}
+    <button
+      className={`${props.isHeld ? 'bg-[#59E391]' : 'bg-white'} m-5 flex items-center justify-center rounded-md px-10 py-8 text-3xl font-bold hover:cursor-pointer hover:bg-[#59E391]`}
+    >
+      {props.value}
     </button>
   );
 }
